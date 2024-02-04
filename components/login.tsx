d7f2c148f0970c0a82e4fef294712f9264401b3f
Monfernape <engr.usmankhalil@gmail.com>
2024-02-05 01:29:32 +0500

    fix: added login page

---
+++ components/login.tsx
@@ -0,0 +1,47 @@
+import {
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+  Card,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export function Login() {
+  return (
+    <div className="w-full h-full">
+      <Card className="mx-auto max-w-sm">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl font-bold">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                placeholder="m@example.com"
+                required
+                type="email"
+              />
+            </div>
+            <div className="space-y-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <Input id="password" required type="password" />
+            </div>
+            <Button className="w-full" type="submit">
+              Login
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
